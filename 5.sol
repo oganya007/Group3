751c2c17bce5e54b7487765dbb4b6973785b1a18
oganya007 <113994623+oganya007@users.noreply.github.com>
2022-10-02 13:59:38 +0100

    Create 5.sol

---
+++ 5.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract tech4dev {
+
+//Create a function that consist of if and else statement, and give an example with it.
+
+  function Mihigo(uint _x) public pure returns(uint){
+
+      if (_x > 30){
+          return 5;
+          
+      } else {
+          return 7;
+      }
+  }
+}
