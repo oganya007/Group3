d54973984bd295cff45391436bb7a65e3012356e
oganya007 <113994623+oganya007@users.noreply.github.com>
2022-10-02 13:35:39 +0100

    Create 2.sol

---
+++ 2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Joy {
+
+//Give 2 examples of the following: fixed array and dynamic array.
+//fixed array
+string [2] public Joy = ["come", "door"];
+uint [3] public Ifure = [1, 2, 3];
+
+//dynamic array
+uint [] public Hailu = [4, 5, 6, 7];
+uint [] public Halimat = [8, 9, 10, 11, 12];
+
+}
